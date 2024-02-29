a64ea259c6af436c08bc29c6d8e960d3ad6b1bbd
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-02-29 11:31:54 +0800

    Initial commit

---
+++ Assets/Assemblies/Manager/GlobalManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityToolkit;
+
+namespace Assemblies
+{
+    public class GlobalManager : MonoSingleton<GlobalManager>
+    {
+        protected override bool DontDestroyOnLoad() => true;
+        [field: SerializeField] public Camera camera { get; private set; }
+    }
+}
---
+++ Assets/Plugins/Unity-Toolkit/Engine/UIFramework/IUIComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityToolkit
+{
+    //用于标记的接口 表示UI组件
+    public interface IUIComponent
+    {
+        
+    }
+    
+    public interface IUISubPanel : IUIComponent
+    {
+        void Open();
+        void Close();
+    }
+}
