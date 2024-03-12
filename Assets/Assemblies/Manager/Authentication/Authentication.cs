18b29886edce52521ab49672b1ffd021d50d6f70
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-03-12 17:33:21 +0800

    feat: 调试HUD

--- Assets/Assemblies/Manager/Authentication/Authentication.cs
+++ Assets/Assemblies/Manager/Authentication/Authentication.cs
@@ -4,14 +4,12 @@ using UnityToolkit;
 
 namespace Game
 {
-    public class Authentication : MonoSingleton<Authentication>
-    {
-        [SerializeField] public string _playerName;
-        [SerializeField] public string _userId;
-        [SerializeField] public string _password;
-        public static string userId => SingletonNullable._userId;
-        public static string playerName => SingletonNullable._playerName;
 
-        protected override bool DontDestroyOnLoad() => false;
+    public static class Authentication
+    {
+        public static string userId;
+        public static string playerName;
+
+
     }
 }
